847c8ca09842cb146850f6f48ea5c8f1cb2af279
Alexander Hoinville <ahoin001@gmail.com>
2019-11-29 23:29:19 -0500

    Attempted fix using axios calls fromm external file, still seeking help for this issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
 import Autocomplete from './components/AutoCompInput';
+
+import './App.css';
+
+import { getPlayerByName } from './API CALLLS/NBA.API'
+
 const nba = require('nba-api-client');
 // import Team from './components/team';
 // import Player from './components/player';
@@ -39,7 +43,7 @@ class App extends React.Component {
   // * Recives a player object and saves it in state
   getSelectedPlayer(playerName, e) {
 
-    console.log('SELECTED PLAYER:', playerName);
+    console.log('USER ENTERED PLAYER:', playerName);
 
     this.setState({
       currentPlayerFullName: playerName
@@ -53,64 +57,8 @@ class App extends React.Component {
     let playerLastName = (playerName.split(" ")[1]);
     console.log('Player Last Name: ', playerLastName);
 
-    // First return list of all players with this first name
-    axios({
-      "method": "GET",
-      "url": `https://api-nba-v1.p.rapidapi.com/players/firstName/${playerFirstName}`,
-      "headers": {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-        "x-rapidapi-key": "4cec6170bcmsh5d6a0ea78315a5ep10f15cjsn59ef0231e8e4"
-      }
-    })
-      .then((playerList) => {
-
-        console.log('PLAYERS WITH THIS FIRST NAME');
-        console.log(playerList.data.api.players)
-
-        // * Then Get list of players with first name that also match provided last name
-        console.log('ABOUT TO GET PLAYERS WITH THIS FIRST NAME AND LAST NAME');
-        var result = playerList.data.api.players.filter(players => {
-          return players.lastName === playerLastName;
-        })
-        console.log('RESULT OF FINDING LAST NAME: ', result);
-        console.log(`THE PLAYER WITH THIS FULL NAME `, result[0]);
-
-        // TODO WHY IS IT GETTTING AN ERROR FROM PAYER ID BUT NOT HARD CODED
-        // // **********************************************************
-        // // DESTRUCTURE TO GET PLAYER ID FROM OBJECT RETURNED FROM API
-
-        // console.log('Name In State: ' +this.state.currentPlayerFullName + 'Name in local: ' + playerName);
-
-        // let { PlayerID, TeamID } = nba.getPlayerID('LaMarcus Aldridge');
-        // console.log(`PlayerID: ${PlayerID} TeamID: ${TeamID}`);
-
-        // // Call to api requires player AND team id
-        // let pic = nba.getPlayerHeadshotURL({ PlayerID: PlayerID, TeamID: TeamID })
-
-        // console.log(pic);
-        // this.setState({
-        //   playerHeadshot: nba.getPlayerHeadshotURL({ PlayerID: PlayerID, TeamID: TeamID })
-        // })
-
-        // // **********************************************************
-
-        // * Save Player In state For Use
-        this.setState({
-          currentPlayer: result[0]
-        })
-
-        console.log(this.state.currentPlayer.country);
-        console.log(this.state.currentPlayer.dateOfBirth);
-
-        this.getPlayerTeam(this.state.currentPlayer.teamId)
-        // this.getPlayerHeadshot(playerName)
-
-
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    // Funtion Returns Plaer with this First and Last Name
+    getPlayerByName(playerFirstName,playerLastName);    
 
   }
 
