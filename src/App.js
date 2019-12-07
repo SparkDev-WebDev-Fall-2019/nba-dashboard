a2f4d326f1488b2492edb49c7769b6ca1147cce3
Alexander Hoinville <ahoin001@gmail.com>
2019-12-07 13:26:19 -0500

    Refactor and add more stat averages,create component for boxscores

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-/* TODO
+/* 
+
+TODO
 1. Input validation
 2. Why Is chart Not showing up?
 3. After Calls are set, decide on layout
@@ -6,20 +8,20 @@
 5. Add Logic to deal with missed games so players don't have 0's
 6.Conditionally Render chart
 7.Change Auto Complete too the suggestion instead of just adding suggestion
+8. Inform user that if not ten ga,es shown, they sat out those games
+9. MPg for averages keeps sayin NaN
 
 */
-
-// Packages
 import React from 'react';
-
 
 // Components
 import Autocomplete from './components/AutoCompInput';
+
+
 import Team from './components/Team';
 import Player from './components/Player';
-import Statbox from './components/Statbox';
+import StatboxAverages from './components/statbox/StatboxAverages';
 import LineChart from './components/LineChart';
-
 
 import './App.css';
 
@@ -28,6 +30,9 @@ import { getPlayerTeam } from './API CALLLS/NBA.API'
 import { getPlayerStats } from './API CALLLS/NBA.API'
 
 const nba = require('nba-api-client');
+
+// let SimpleSelect = require("react-selectize").SimpleSelect
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -44,10 +49,9 @@ class App extends React.Component {
       playerPPGArray: [],
       playerRPG: 0,
       playerAPG: 0,
+      lastTenGamesAverages: {}
 
     };
-
-
 
   }
 
@@ -66,7 +70,7 @@ class App extends React.Component {
 
   async getSelectedPlayerInfo(playerName, e) {
 
-    // To Hold Values and not have to repeatedly call state after to reference data
+    // Hold Values and not have to repeatedly call state after to reference data
     let stateObject = {};
 
     let playerFirstName = (playerName.split(" ")[0]);
@@ -81,7 +85,7 @@ class App extends React.Component {
     await getPlayerByName.call(this, playerFirstName, playerLastName)
       .then(foundPlayer => {
 
-        console.log('I AM RETURNED FROM GETPLAYER', foundPlayer);
+        // console.log('I AM RETURNED FROM GETPLAYER', foundPlayer);
         stateObject.currentPlayer = foundPlayer
       })
 
@@ -101,50 +105,133 @@ class App extends React.Component {
 
         stateObject.playerTeam = foundTeam
 
-        console.log(`THE PLAYERS TEAM`, foundTeam);
+        // console.log(`THE PLAYERS TEAM`, foundTeam);
 
-        console.log(this.state);
+        // console.log(this.state);
+        // console.log(this.stateObject);
 
       })
 
     await getPlayerStats.call(this, stateObject.currentPlayer.playerId)
-      .then(lastTenGames => {
+      .then(res => {
 
-        console.log(lastTenGames);
+        // Data containing averages and stats of last 10 games
+        let lastTenGamesAverages = {}
+
         let pointsSum = 0;
-        let pointsArray = [];
+        lastTenGamesAverages.pointsArray = []
 
         let reboundsSum = 0;
+        lastTenGamesAverages.reboundsArray = []
+
         let assistsSum = 0;
+        lastTenGamesAverages.assistsArray = []
 
-        lastTenGames.forEach(game => {
-          pointsSum += Number(game.points)
-          pointsArray.push(Number(game.points))
+        let stealsSum = 0;
+        lastTenGamesAverages.stealsArray = []
 
-          reboundsSum += Number(game.defReb) + Number(game.offReb)
+        let blocksSum = 0;
+        lastTenGamesAverages.blocksArray = []
+
+        let turnOversSum = 0;
+        lastTenGamesAverages.turnOversArray = []
+
+        let fgASum = 0;
+        lastTenGamesAverages.fgAArray = []
+
+        let fgMSum = 0;
+        lastTenGamesAverages.fgMArray = []
+
+        let fTASum = 0;
+        lastTenGamesAverages.ftAArray = []
+
+        let fTMSum = 0;
+        lastTenGamesAverages.ftMArray = []
+
+        let minutesSum = 0;
+        lastTenGamesAverages.minutesArray = []
 
 
-          assistsSum += Number(game.assists)
+        res.forEach(game => {
+
+          console.log(game);
+          if (game.min === "" || game.min === Number("")) {
+            console.log("Did not play this game");
+            console.log(game.min);
+
+          }
+          else {
+
+            pointsSum += Number(game.points)
+            lastTenGamesAverages.pointsArray.push(Number(game.points))
+
+            reboundsSum += Number(game.defReb) + Number(game.offReb)
+            lastTenGamesAverages.reboundsArray.push(Number(game.defReb) + Number(game.offReb))
+
+            assistsSum += Number(game.assists)
+            lastTenGamesAverages.assistsArray.push(Number(game.assists))
+
+            stealsSum += Number(game.steals)
+            lastTenGamesAverages.stealsArray.push(Number(game.steals))
+
+            blocksSum += Number(game.blocks)
+            lastTenGamesAverages.blocksArray.push(Number(game.blocks))
+
+            turnOversSum += Number(game.turnovers)
+            lastTenGamesAverages.turnOversArray.push(Number(game.turnovers))
+
+            fgASum += Number(game.fga)
+            lastTenGamesAverages.fgAArray.push(Number(game.fga))
+
+            fgMSum += Number(game.fgm)
+            lastTenGamesAverages.fgMArray.push(Number(game.fgm))
+
+            fTASum += Number(game.fta)
+            lastTenGamesAverages.ftAArray.push(Number(game.fta))
+
+            fTMSum += Number(game.ftm)
+            lastTenGamesAverages.ftMArray.push(Number(game.ftm))
+            
+            // minutesSum += Number(game.min)
+            // lastTenGamesAverages.minutesArray.push(Number(game.min))
+
+
+          }
+
+
 
         });
 
-        stateObject.playerPPG = pointsSum / lastTenGames.length
-        stateObject.playerPPGArray = pointsArray
+        lastTenGamesAverages.playerPPG = pointsSum / res.length
+        lastTenGamesAverages.playerRPG = reboundsSum / res.length
+        lastTenGamesAverages.playerAPG = assistsSum / res.length
+        lastTenGamesAverages.playerSPG = stealsSum / res.length
+        lastTenGamesAverages.playerBPG = blocksSum / res.length
+        lastTenGamesAverages.playerTOPG = turnOversSum / res.length
 
-        stateObject.playerRPG = reboundsSum / lastTenGames.length
+        lastTenGamesAverages.playerFGAPG = fgASum / res.length
+        lastTenGamesAverages.playerFGMPG = fgMSum / res.length
+        lastTenGamesAverages.FGPercent =   fgMSum / fgASum
 
+        lastTenGamesAverages.playerFTAPG = fTASum / res.length
+        lastTenGamesAverages.playerFTAMPG = fTMSum / res.length
+        lastTenGamesAverages.FTPercent =   fTMSum / fTASum
 
-        stateObject.playerAPG = assistsSum / lastTenGames.length
+        // lastTenGamesAverages.playerMPG = minutesSum / res.length
+
+        console.log(lastTenGamesAverages);
+
+        stateObject.lastTenGamesAverages = lastTenGamesAverages
 
       })
 
-
-    this.setState(stateObject)
+    this.setState(stateObject, () => console.log('The State', this.state))
 
   }
 
   render() {
 
+    let lastTenGames = this.state.lastTenGames
 
     return (
 
@@ -155,7 +242,6 @@ class App extends React.Component {
         <Autocomplete changePlayerNameInState={event => this.genericSync(event)} />
 
         <button
-          // onClick={(e) => { this.getSelectedPlayerInfo(e, this.state.playerName) }}
           onClick={this.getSelectedPlayerInfo.bind(this, this.state.searchedPlayersName)}
         >
           Click Me!
@@ -165,14 +251,16 @@ class App extends React.Component {
 
         <Player Player={this.state.currentPlayerFullName} />
 
-        <Statbox
-          PPG={this.state.playerPPG}
-          RPG={this.state.playerRPG}
-          APG={this.state.playerAPG}
+        <StatboxAverages
+          // Averages= {lastTenGames}
+          PPG={this.state.lastTenGamesAverages.playerPPG}
+          RPG={this.state.lastTenGamesAverages.playerRPG}
+          APG={this.state.lastTenGamesAverages.playerAPG}
+
         />
 
-
-        <LineChart LastTenGames={this.state.playerPPGArray} />
+        {/* LastTenGames Is object containing selected players stats */}
+        <LineChart LastTenGames={this.state.lastTenGamesAverages} />
 
       </div>
 
